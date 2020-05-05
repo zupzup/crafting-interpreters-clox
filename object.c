79d99879c9af4cbe25f9ec92839f4ae92fe56468
Mario Zupan <mario@zupzup.org>
2020-05-05 15:22:08 +0200

    functions basics

--- object.c
+++ object.c
@@ -11,36 +11,45 @@
     (type*)allocateObject(sizeof(type), objectType)
 
 static Obj* allocateObject(size_t size, ObjType type) {
-  Obj* object = (Obj*)reallocate(NULL, 0, size);
-  object->type = type;
+    Obj* object = (Obj*)reallocate(NULL, 0, size);
+    object->type = type;
 
-  object->next = vm.objects;
-  vm.objects = object;
+    object->next = vm.objects;
+    vm.objects = object;
 
-  return object;
+    return object;
+}
+
+ObjFunction* newFunction() {
+    ObjFunction* function = ALLOCATE_OBJ(ObjFunction, OBJ_FUNCTION);
+
+    function->arity = 0;
+    function->name = NULL;
+    initChunk(&function->chunk);
+    return function;
 }
 
 static ObjString* allocateString(char* chars, int length,
-                                 uint32_t hash) {
-  ObjString* string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
-  string->length = length;
-  string->chars = chars;
-  string->hash = hash;
+        uint32_t hash) {
+    ObjString* string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
+    string->length = length;
+    string->chars = chars;
+    string->hash = hash;
 
-  tableSet(&vm.strings, string, NIL_VAL);
+    tableSet(&vm.strings, string, NIL_VAL);
 
-  return string;
+    return string;
 }
 
 static uint32_t hashString(const char* key, int length) {
-  uint32_t hash = 2166136261u;
+    uint32_t hash = 2166136261u;
 
-  for (int i = 0; i < length; i++) {
-    hash ^= key[i];
-    hash *= 16777619;
-  }
+    for (int i = 0; i < length; i++) {
+        hash ^= key[i];
+        hash *= 16777619;
+    }
 
-  return hash;
+    return hash;
 }
 
 ObjString* takeString(char* chars, int length) {
@@ -55,21 +64,32 @@ ObjString* takeString(char* chars, int length) {
 }
 
 ObjString* copyString(const char* chars, int length) {
-  uint32_t hash = hashString(chars, length);
-  ObjString* interned = tableFindString(&vm.strings, chars, length,
-          hash);
-  if (interned != NULL) return interned;
+    uint32_t hash = hashString(chars, length);
+    ObjString* interned = tableFindString(&vm.strings, chars, length,
+            hash);
+    if (interned != NULL) return interned;
 
-  char* heapChars = ALLOCATE(char, length + 1);
-  memcpy(heapChars, chars, length);
-  heapChars[length] = '\0';
-  return allocateString(heapChars, length, hash);
+    char* heapChars = ALLOCATE(char, length + 1);
+    memcpy(heapChars, chars, length);
+    heapChars[length] = '\0';
+    return allocateString(heapChars, length, hash);
+}
+
+static void printFunction(ObjFunction* function) {
+    if (function->name == NULL) {
+        printf("<script>");
+        return;
+    }
+    printf("<fn %s>", function->name->chars);
 }
 
 void printObject(Value value) {
-  switch (OBJ_TYPE(value)) {
-    case OBJ_STRING:
-      printf("%s", AS_CSTRING(value));
-      break;
-  }
+    switch (OBJ_TYPE(value)) {
+        case OBJ_FUNCTION:
+            printFunction(AS_FUNCTION(value));
+            break;
+        case OBJ_STRING:
+            printf("%s", AS_CSTRING(value));
+            break;
+    }
 }
